a62af4d39c036a55fa1742584ba073e96b1f2f24
Andris Andermann <andris.andermann@terra.com.br>
2022-12-07 14:58:49 -0300

    Recipe list design

--- shopping-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
 
   recipes: Recipe[] = [
+    new Recipe('A test Recipe','This is simply a test', 'https://lh3.googleusercontent.com/m4nTw90Qpnb_BZwENHBv3FjfJ6Xt2mpgnP-bCr8j_1_fOXmqN000XjTh5B__ZpTt9BKWekysBSDhNljGPJPk799bBIkw4iFbAg=w220-h220-c-rw-v1-e365'),
+    new Recipe('A test Recipe','This is simply a test', 'https://lh3.googleusercontent.com/m4nTw90Qpnb_BZwENHBv3FjfJ6Xt2mpgnP-bCr8j_1_fOXmqN000XjTh5B__ZpTt9BKWekysBSDhNljGPJPk799bBIkw4iFbAg=w220-h220-c-rw-v1-e365'),
     new Recipe('A test Recipe','This is simply a test', 'https://lh3.googleusercontent.com/m4nTw90Qpnb_BZwENHBv3FjfJ6Xt2mpgnP-bCr8j_1_fOXmqN000XjTh5B__ZpTt9BKWekysBSDhNljGPJPk799bBIkw4iFbAg=w220-h220-c-rw-v1-e365')
   ];
 
