488cf9de5935413f69f186607ffc7dd3b311962a
Andris Andermann <andris.andermann@terra.com.br>
2022-12-07 14:31:44 -0300

    Update recipes.component.ts

--- shopping-recipe-app/src/app/recipes/recipes.component.ts
+++ shopping-recipe-app/src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +6,5 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent {
-
-  recipes: Recipe[] = [
-    new Recipe('A test Recipe','This is simply a test', 'https://lh3.googleusercontent.com/m4nTw90Qpnb_BZwENHBv3FjfJ6Xt2mpgnP-bCr8j_1_fOXmqN000XjTh5B__ZpTt9BKWekysBSDhNljGPJPk799bBIkw4iFbAg=w220-h220-c-rw-v1-e365')
-  ];
 
 }
